eb5a1a596e487b0a3b17799ef0343023b5a90c7d
komeilyeganeh <komeilyeganeh261@gmail.com>
2024-11-15 15:18:44 +0330

    v1.0.0: starting setup

---
+++ index.js
@@ -0,0 +1,9 @@
+// :::: import express ::::
+import express from "express";
+
+// :::: create express app ::::
+const app = express();
+
+
+// :::: required port to listening ::::
+app.listen(4000);
